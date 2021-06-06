d61d75eee947b8d92f09662553637684fa9a17c3
floX <flornalix@gmail.com>
2021-06-06 16:49:45 +0200

    Super tolles Projekt erstellt

---
+++ Students-Java/src/main/java/de/students/main/Main.java
@@ -0,0 +1,15 @@
+package de.students.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void start(Stage stage) throws Exception {
+
+    }
+}
