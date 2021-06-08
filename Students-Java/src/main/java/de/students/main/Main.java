33fe47d52a128ae34befda5ec9ea35237de487ce
floxikoxi <flornalix@gmail.com>
2021-06-08 11:14:31 +0200

    JavaFX zum laufen gebracht mit kleinem Fenster

--- Students-Java/src/main/java/de/students/main/Main.java
+++ Students-Java/src/main/java/de/students/main/Main.java
@@ -1,15 +1,23 @@
 package de.students.main;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 
+    @Override
     public void start(Stage stage) throws Exception {
+
+        Label label = new Label("Hello World! :)");
+        stage.setScene(new Scene(label, 250, 100));
+        stage.setTitle("Hello JavaFX");
+        stage.show();
 
     }
 }
