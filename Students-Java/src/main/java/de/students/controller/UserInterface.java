bc9dde01c1c278735e8be132980515674d8ffca2
floxikoxi <flornalix@gmail.com>
2021-06-11 09:47:24 +0200

    .fxml added id to table and tablecollum

--- Students-Java/src/main/java/de/students/controller/UserInterface.java
+++ Students-Java/src/main/java/de/students/controller/UserInterface.java
@@ -1,10 +1,16 @@
 package de.students.controller;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 public class UserInterface{
+
+    @FXML
+    TableView kursTable;
+
 }
