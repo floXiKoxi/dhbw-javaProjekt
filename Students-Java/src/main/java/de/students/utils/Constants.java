3849201faecdbf5a2e7273b015fb974f9e35416a
floX <flornalix@gmail.com>
2021-07-01 17:47:56 +0200

    Code Duplizierung

--- Students-Java/src/main/java/de/students/utils/Constants.java
+++ Students-Java/src/main/java/de/students/utils/Constants.java
@@ -2,17 +2,25 @@ package de.students.utils;
 
 public class Constants {
 
-    public static final String[] ALLOWED_CHARACTERS = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    public static final String[] ALLOWED_CHARACTERS_MATRIKEL = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
     public static boolean isCorrect(String matrikel){
 
-        for(String character : ALLOWED_CHARACTERS){
+        for(String character : ALLOWED_CHARACTERS_MATRIKEL){
 
-            if(matrikel.contentEquals(character)){
-                return true;
+            if(matrikel.contentEquals(character) || matrikel.length() != 7){
+                return false;
             }
 
         }
+        return true;
+
+    }
+
+    public static boolean isCorrectName(String name){
+
+        if(name.matches("^[a-zA-Z]+$") && name.length() > 0) return true;
+
         return false;
 
     }
---
+++ Students-Java/src/main/java/de/students/utils/StageLoader.java
@@ -0,0 +1,120 @@
+package de.students.utils;
+
+import de.students.controller.CoursePopupController;
+import de.students.controller.StudentPopupController;
+import de.students.entity.Kurs;
+import de.students.entity.Student;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StageLoader {
+
+    private FXMLLoader fxmlLoader;
+    private Parent parent;
+    private Stage stage;
+
+    public StageLoader(String fxml_file){
+
+        setFxmlLoader(fxml_file);
+
+    }
+
+    public void openStage(){
+
+        try {
+            setParent(getFxmlLoader().load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(getParent()));
+        stage.showAndWait();
+        stage.setAlwaysOnTop(true);
+
+        setStage(stage);
+
+    }
+    public void openStageEdit(Kurs kurs){
+
+        try {
+            setParent(getFxmlLoader().load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(getParent()));
+        CoursePopupController controller = getFxmlLoader().getController();
+        controller.initData(kurs);
+        //Wenn Stage geschlossen ist, dann macht weiter mit code.
+        stage.showAndWait();
+        stage.setAlwaysOnTop(true);
+
+        setStage(stage);
+
+    }
+    public void openStageEditStudent(Student student){
+
+        try {
+            setParent(getFxmlLoader().load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(getParent()));
+        StudentPopupController controller = getFxmlLoader().getController();
+        controller.initData(student);
+        //Wenn Stage geschlossen ist, dann macht weiter mit code.
+        stage.setAlwaysOnTop(true);
+        stage.showAndWait();
+
+        setStage(stage);
+
+    }
+    public void openStage(String title){
+
+        try {
+            setParent(getFxmlLoader().load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(getParent()));
+        stage.show();
+        stage.setTitle(title);
+        stage.setAlwaysOnTop(true);
+
+        setStage(stage);
+    }
+
+    public Parent getParent() {
+        return parent;
+    }
+
+    public void setParent(Parent parent) {
+        this.parent = parent;
+    }
+
+    public FXMLLoader getFxmlLoader(){
+
+        return this.fxmlLoader;
+    }
+    public void setFxmlLoader(String name){
+        this.fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource(name));
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+}
